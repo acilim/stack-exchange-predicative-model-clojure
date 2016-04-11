cee662e50de5beec3a23354ef5314be969693676
Milica Ilic <milica.ilic92@yahoo.com>
2016-04-11 18:00:50 +0200

    Added tests for question-processing functions

--- test/app/test/test.clj
+++ test/app/test/test.clj
@@ -3,15 +3,52 @@
   (:use [helloclojureapp.data-processing]))
 
 (def testQuestion 
-  "{        \"body\": \"The quick brown fox jumps over the lazy dog.\", 
-            \"owner\": { \"user_id\": 655490, }, 
-            \"question_id\": 24617665, 
-            \"score\": 0, 
-            \"tags\": [\"tag1\", \"tag2\", \"tag3\"], 
-            \"title\": \"Java, Where to place public enums\"
-        }")
+  {:question_id 24617665
+   :title "TestQuestion" 
+   :body "Question body. <code>123456</code> Here are some special characters: !#%^&*( and some more code: <code>789</code>.\nLet's also have some urls: <a href=...>url1</a> <a href='otherurl'>...</a> And one stackoverflow/questions/... url."
+   :owner {:user_id 655490} 
+   :tags ["first" "second" "third"]
+   :score 12 })
 
-(println (getNumberOfShortWords testQuestion))
 
-(deftest testGetNumberOfShortWords 
-  (is  (getNumberOfShortWords testQuestion )))
+(deftest test-number-of-urls 
+  (is
+    (getNumberOfURLs testQuestion 2)))
+
+(deftest test-number-of-stackoverflow-urls 
+  (is
+    (getNumberOfURLs testQuestion 1)))
+
+(deftest test-title-length 
+  (is
+    (getTitleLength testQuestion 12)))
+
+(deftest test-body-length 
+  (is
+    (getBodyLength testQuestion 230)))
+
+(deftest test-num-of-tags
+  (is
+    (getNumberOfTags testQuestion 3)))
+
+(deftest test-num-of-punctuation_marks
+  (is
+    (getNumberOfPunctuationMarks testQuestion 16)))
+
+(deftest test-num-of-short-words
+  (is
+    (getNumberOfShortWords testQuestion 6)))
+
+(deftest test-num-of-lowercase-letters
+  (is
+    (getNumberOfLowercaseLetters testQuestion 141)))
+
+(deftest test-num-of-uppercase-letters
+  (is
+    (getNumberOfUppercaseLetters testQuestion 4)))
+
+(deftest test-code-snippet-length
+  (is
+    (getNumberOfUppercaseLetters testQuestion 9)))
+
+
