01f4161f1a7d40cfbe84c628af19957a829a6deb
Milica Ilic <milica.ilic92@yahoo.com>
2016-04-11 19:26:22 +0200

    Delete data-classification.clj

--- bin/src/helloclojureapp/data-classification.clj
+++
@@ -1,6 +0,0 @@
-(ns helloclojureapp.data-classification
-  (:require 
-            [clojure.data.csv :as csv])
-  (:use  [weka.core.converters ArffLoader CSVLoader]
-         [clojure.java.io :as io]))
-
