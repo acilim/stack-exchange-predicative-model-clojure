bc67abc7db92ee78dbf1ae8a1f4c1fcf2477d438
Milica Ilic <milica.ilic92@yahoo.com>
2016-04-12 13:37:31 +0200

    Added files via upload

---
+++ src/helloclojureapp/main-class.clj
@@ -0,0 +1,10 @@
+(ns helloclojureapp.data-processing
+  (:use [helloclojureapp.data-collection]
+        [helloclojureapp.data-processing]
+        [helloclojureapp.data-classification]))
+
+;;(collect-questions)
+;;(create-training-dataset)
+;;(create-test-dataset)
+;;(train-classifiers)
+;;(save-results)
