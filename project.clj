e206d4067ca0489e446442d3e43679a44211ef0c
Milica Ilic <milica.ilic92@yahoo.com>
2016-04-12 13:38:11 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject helloclojureapp "0.1.0-SNAPSHOT"
-  :description "FIXME: I WAS NEVER BROKEN"
-  :url "http://www.greatbigstuff.com/"
+  :description "Prediction model that uses StackExchange API and predicts whether the question asked pon stackoverflow would be closed or not."
+  :url "https://github.com/acilim/stack-exchange-predicative-model-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
@@ -9,4 +9,5 @@
                  [clj-http "0.6.0"]
                  [ring/ring-json "0.4.0"]
                  [nz.ac.waikato.cms.weka/weka-dev "3.7.7"]
-                 [cc.artifice/clj-ml "0.6.1"]])
+                 [cc.artifice/clj-ml "0.6.1"]]
+    :main helloclojureapp.main-class)
