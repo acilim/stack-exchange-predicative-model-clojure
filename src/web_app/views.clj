14a872a079137a608ba51f123877d5a42c8b0e14
Milica Ilic <milica.ilic92@yahoo.com>
2016-06-02 10:06:48 +0200

    Update views.clj

--- src/web_app/views.clj
+++ src/web_app/views.clj
@@ -1,20 +1,23 @@
 (ns web_app.views
   (:require [clojure.string :as str]
-            [hiccup.page :as hic-p])
+            [hiccup.page :as page])
   (:use [machinelearning.data-classification]))
 
 (defn home-page
   []
-  (hic-p/html5
+  (page/html5
     [:head
      [:title (str "Stack exchange predicative model")]
-     (hic-p/include-css "/css/style.css" "/css/reset.css")]   
+     (page/include-css "/css/style.css" "/css/reset.css")
+     (page/include-js "/js/validation.js")]   
      [:div {:class "basic-grey"
             :style "display:block"}
       [:h1 "Classify your question! "]
       [:h2 "Enter data:"]
       [:form {:action "/classify"
-              :method "POST"} 
+              :method "POST"
+              :name "qForm"
+              :onsubmit "return validateForm()"} 
        [:label "Your user id:"]
        [:input {:type "text"
                 :name "user-id" 
@@ -29,13 +32,11 @@
 (defn results-page
   [{:keys [text user-id]}]
   (let [closed (classify user-id text)]
-    (hic-p/html5
+    (page/html5
       [:head
        [:title (str "Stack exchange predicative model")]
-       (hic-p/include-css "/css/style.css" "/css/reset.css")]  
+       (page/include-css "/css/style.css" "/css/reset.css")]  
       [:div {:class "basic-grey"}
        [:h1 "Your result:"]  
        [:h2 closed]      
        [:a {:href "/"} "Classify another"]])))
-
-
